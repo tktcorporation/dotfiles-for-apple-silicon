51994465cfbec8a2e42674bbf9b32ec288f00e58
tktcorporation <tktcorporation.go@gmail.com>
2021-01-14 16:25:13 +0900

    feat: set alias to use aws command with docker

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -14,3 +14,7 @@ set -g theme_display_git_master_branch yes  # git の branch 名を表示
 set -g theme_color_scheme dracula
 set -g theme_display_date no  # 時刻を表示しないように設定
 set -g theme_display_cmd_duration no  # コマンド実行時間の非表示
+
+# alias
+
+alias aws "docker run --rm -it -v ~/.aws:/root/.aws -v (pwd):/aws amazon/aws-cli"
