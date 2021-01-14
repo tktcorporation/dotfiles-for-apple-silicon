d1520e95edee77beaa238d3827dde244a7187206
tktcorporation <tktcorporation.go@gmail.com>
2021-01-14 16:47:58 +0900

    feat: add config to use direnv

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -15,6 +15,10 @@ set -g theme_color_scheme dracula
 set -g theme_display_date no  # 時刻を表示しないように設定
 set -g theme_display_cmd_duration no  # コマンド実行時間の非表示
 
+# direnv
+direnv hook fish | source
+set -x EDITOR code-insiders
+
 # alias
 
 alias aws "docker run --rm -it -v ~/.aws:/root/.aws -v (pwd):/aws amazon/aws-cli"
